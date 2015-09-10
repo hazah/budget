3e2f90f9e9e79ebb76720b676f221d61e1da248a
Ivgeni Slabkovski <zhenya@zhenya.ca>
2015-09-09 23:20:01 -0400

    Adds account tests.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,6 @@
 class Account < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :account_classification, presence: true
+
   belongs_to :account_classification
 end
