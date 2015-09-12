c99afb9d10c0960355cf37d6d81b0727b57a3133
Ivgeni Slabkovski <zhenya@zhenya.ca>
2015-09-12 12:16:19 -0400

    Adjusts foreign key definition for entries.

--- db/migrate/20150911030736_create_entries.rb
+++ db/migrate/20150911030736_create_entries.rb
@@ -1,8 +1,8 @@
 class CreateEntries < ActiveRecord::Migration
   def change
     create_table :entries do |t|
-      t.references :debit, index: true, foreign_key: true, null: false
-      t.references :credit, index: true, foreign_key: true, null: false
+      t.references :debit, index: true, foreign_key: { table_name: :account }, null: false
+      t.references :credit, index: true, foreign_key: { table_name: :account }, null: false
       t.decimal :amount, precision: 10, scale: 2, null: false
       t.date :transaction_date, null: false
 
